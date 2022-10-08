586116bec37412a26549122baf14725ac0e0f7f9
ramirez <prlara2000@gmail.com>
2022-10-08 15:37:53 -0500

    add filemon

--- src/bible/filemon.js
+++ src/bible/filemon.js
@@ -1,2 +1,29 @@
-const filemon = {};
+const filemon = {
+  1: {
+    1: " Pablo, prisionero de Jesucristo, y el hermano Timoteo, al amado Filemón, colaborador nuestro,\n",
+    2: " y a la amada hermana Apia, y a Arquipo nuestro compañero de milicia, y a la iglesia que está en tu casa:\n",
+    3: " Gracia y paz a vosotros, de Dios nuestro Padre y del Señor Jesucristo.\n",
+    4: " Doy gracias a mi Dios, haciendo siempre memoria de ti en mis oraciones,\n",
+    5: " porque oigo del amor y de la fe que tienes hacia el Señor Jesús, y para con todos los santos;\n",
+    6: " para que la participación de tu fe sea eficaz en el conocimiento de todo el bien que está en vosotros por Cristo Jesús.\n",
+    7: " Pues tenemos gran gozo y consolación en tu amor, porque por ti, oh hermano, han sido confortados los corazones de los santos.\n",
+    8: " Por lo cual, aunque tengo mucha libertad en Cristo para mandarte lo que conviene,\n",
+    9: " más bien te ruego por amor, siendo como soy, Pablo ya anciano, y ahora, además, prisionero de Jesucristo;\n",
+    10: " te ruego por mi hijo Onésimo, a quien engendré en mis prisiones,\n",
+    11: " el cual en otro tiempo te fue inútil, pero ahora a ti y a mí nos es útil,\n",
+    12: " el cual vuelvo a enviarte; tú, pues, recíbele como a mí mismo.\n",
+    13: " Yo quisiera retenerle conmigo, para que en lugar tuyo me sirviese en mis prisiones por el evangelio;\n",
+    14: " pero nada quise hacer sin tu consentimiento, para que tu favor no fuese como de necesidad, sino voluntario.\n",
+    15: " Porque quizás para esto se apartó de ti por algún tiempo, para que le recibieses para siempre;\n",
+    16: " no ya como esclavo, sino como más que esclavo, como hermano amado, mayormente para mí, pero cuánto más para ti, tanto en la carne como en el Señor.\n",
+    17: " Así que, si me tienes por compañero, recíbele como a mí mismo.\n",
+    18: " Y si en algo te dañó, o te debe, ponlo a mi cuenta.\n",
+    19: " Yo Pablo lo escribo de mi mano, yo lo pagaré; por no decirte que aun tú mismo te me debes también.\n",
+    20: " Sí, hermano, tenga yo algún provecho de ti en el Señor; conforta mi corazón en el Señor.\n",
+    21: " Te he escrito confiando en tu obediencia, sabiendo que harás aun más de lo que te digo.\n",
+    22: " Prepárame también alojamiento; porque espero que por vuestras oraciones os seré concedido.\n",
+    23: " Te saludan Epafras, mi compañero de prisiones por Cristo Jesús,\n",
+    24: " Marcos, Aristarco, Demas y Lucas, mis colaboradores.\n",
+  },
+};
 export default filemon;
